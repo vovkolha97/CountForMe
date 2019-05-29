0607e6daf2b3c9715a2882a0b290dc7698db4530
Olha Vovk <vovkolha97@gmail.com>
2019-05-29 22:57:25 +0300

    initial commit

--- CountForMe/ViewController.swift
+++ CountForMe/ViewController.swift
@@ -10,7 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func twoBtn(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func zero(_ sender: Any) {
+    }
+    @IBAction func devide(_ sender: Any) {
+    }
+    @IBAction func multiple(_ sender: Any) {
+    }
+    @IBAction func minus(_ sender: Any) {
+    }
+    @IBAction func plus(_ sender: Any) {
+    }
+    @IBAction func equal(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
