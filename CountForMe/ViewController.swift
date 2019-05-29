3f5d54d840fb63cf94abe12e27a177d20a390968
Olha Vovk <vovkolha97@gmail.com>
2019-05-29 21:32:37 +0300

    Initial Commit

---
+++ CountForMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountForMe
+//
+//  Created by Вовк Ольга' on 5/29/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
