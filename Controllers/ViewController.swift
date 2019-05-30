2a76a4d4140d3c73c00de3d7d5b3e3d2c0f4ace6
Olha Vovk <vovkolha97@gmail.com>
2019-05-30 22:20:19 +0300

    second argument added

--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -3,72 +3,81 @@ import UIKit
 class ViewController: UIViewController {
      var argument1: Int = 0
      var argument2: Int = 0
-    var operatorBtn:Character?
+    var operatorBtn:Character? = nil
     
     
     @IBOutlet weak var TextField: UITextField!
    
     @IBAction func seven(_ sender: Any) {
-        btnPressed(argument1: 7)
+        btnPressed(digit: 7)
     }
     @IBAction func eight(_ sender: Any) {
-        btnPressed(argument1: 8)
+        btnPressed(digit: 8)
     }
     @IBAction func nine(_ sender: Any) {
-        btnPressed(argument1: 9)
+        btnPressed(digit: 9)
     }
     @IBAction func five(_ sender: Any) {
-        btnPressed(argument1: 5)
+        btnPressed(digit: 5)
     }
     @IBAction func four(_ sender: Any) {
-        btnPressed(argument1: 4)
+        btnPressed(digit: 4)
     }
     @IBAction func six(_ sender: Any) {
-        btnPressed(argument1: 6)
+        btnPressed(digit: 6)
     }
     @IBAction func one(_ sender: Any) {
-        btnPressed(argument1: 1)
+        btnPressed(digit: 1)
     }
     @IBAction func two(_ sender: Any) {
-        btnPressed(argument1: 2)
+        btnPressed(digit: 2)
     }
     @IBAction func three(_ sender: Any) {
-        btnPressed(argument1: 3)
+        btnPressed(digit: 3)
     }
     @IBAction func zero(_ sender: Any) {
-        btnPressed(argument1: 0)
+        btnPressed(digit: 0)
     }
     @IBAction func cleanBtn(_ sender: Any) {
     }
     @IBAction func equal(_ sender: Any) {
     }
     @IBAction func plus(_ sender: Any) {
-        operatorChoosed(operatorBtn: "+")
+        operatorChosed(operatorBtn: "+")
     }
     @IBAction func minus(_ sender: Any) {
-        operatorChoosed(operatorBtn: "-")
+        operatorChosed(operatorBtn: "-")
         
     }
     @IBAction func multiple(_ sender: Any) {
-        operatorChoosed(operatorBtn: "*")
+        operatorChosed(operatorBtn: "*")
         
     }
-    @IBAction func devide(_ sender: Any) {
-        operatorChoosed(operatorBtn: "/")
+    @IBAction func divide(_ sender: Any) {
+        operatorChosed(operatorBtn: "/")
         
     }
   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
-    func btnPressed( argument1: Int) {
-        if TextField.text!.count != 4 {
-          self.argument1 = (self.argument1 * 10) + argument1
-            TextField.text = String(self.argument1)
-        }
+    func btnPressed( digit: Int) {
+            if operatorBtn.self == nil {
+                self.argument1 = (self.argument1 * 10) + digit
+                TextField.text = String(self.argument1)
+                print("nil")
+            } else {
+                self.argument2 = (self.argument2 * 10) + digit
+                TextField.text! = TextField.text! + String(self.argument2)
+                print("not nil")
+            }
   }
-    func operatorChoosed( operatorBtn: Character) {
-        TextField.text! = TextField.text! + String(operatorBtn)
+    func operatorChosed( operatorBtn: Character) {
+        self.operatorBtn = operatorBtn
+        TextField.text = TextField.text! + String(operatorBtn)
     }
+    
+  
 }
